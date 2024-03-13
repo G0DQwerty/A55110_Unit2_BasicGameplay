2fb499790c7f55fd584a061568e923794965f575
David Figueiredo <150687060+G0DQwerty@users.noreply.github.com>
2024-03-13 13:17:34 +0000

    Unit2_Part2.2_End
    Finished all the steps of Lesson 2.2 Food Flight

---
+++ Assets/Scripts/DestroyOutofBounds.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutofBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float bottomBound = 10;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < bottomBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
